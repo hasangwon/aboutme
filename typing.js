9e2ce0a161cee0b0e7afc88833c07069ac94f001
SANGWON HA <bj5578@hs.ac.kr>
2022-05-16 03:29:15 +0900

    0516new

---
+++ typing.js
@@ -0,0 +1,19 @@
+"use strict";
+const content = "저는 하상원입니다"
+const text = document.querySelector(".text")
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(1000);
+    }
+}
+setInterval(typing, 300)
